d4c1ce2b8f1c9ee16a148486f5af51d3edc2de18
Ariya Hidayat <ariya.hidayat@gmail.com>
2023-10-04 16:02:39 -0700

    Step 3: Text generation/completion

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Pico Jarvis
 
-Requirement: Node.js v18 or later.
+Requirement:
+* Node.js v18 or later.
+* [llama.cpp](https://github.com/ggerganov/llama.cpp) running a model, e.g. [Orca Mini 3B](https://huggingface.co/TheBloke/orca_mini_3B-GGML).
 
 Run:
 ```
@@ -8,3 +10,10 @@ $ node pico-jarvis
 ```
 
 and then open `localhost:5000`.
+
+Try some of these examples:
+
+* The CEO of Google is
+* The largest planet is
+* Mona Lisa was painted by
+* The real name of Spiderman is
--- pico-jarvis.js
+++ pico-jarvis.js
@@ -1,7 +1,28 @@
 const fs = require('fs');
 const http = require('http');
 
-function handler(request, response) {
+const LLAMA_API_URL = process.env.LLAMA_API_URL || 'http://127.0.0.1:8080/completion';
+
+async function llama(prompt) {
+    const method = 'POST';
+    const headers = {
+        'Content-Type': 'application/json'
+    };
+    const body = JSON.stringify({
+        prompt: prompt,
+        n_predict: 200,
+        temperature: 0,
+        top_k: 20,
+        stop: ["Llama:", "User:"]
+    });
+    const request = { method, headers, body };
+    const response = await fetch(LLAMA_API_URL, request);
+    const data = await response.json();
+    const { content } = data;
+    return content.trim();
+}
+
+async function handler(request, response) {
     const { url } = request;
     console.log(`Handling ${url}...`);
     if (url === '/health') {
@@ -13,8 +34,9 @@ function handler(request, response) {
         const parsedUrl = new URL(`http://localhost/${url}`);
         const { search } = parsedUrl;
         const question = decodeURIComponent(search.substring(1));
-        const answer = 'Yo man';
-        console.log({ question, answer });
+        console.log('Waiting for Llama...');
+        const answer = await llama(question);
+        console.log('LLama answers:', answer);
         response.writeHead(200).end(answer);
     } else {
         console.error(`${url} is 404!`)

07adcb7233aa358cf792baff912c446677d25c9a
Ariya Hidayat <ariya.hidayat@gmail.com>
2023-10-04 16:05:59 -0700

    Step 2: HTTP server, hardcoded answer

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Pico Jarvis
+
+Requirement: Node.js v18 or later.
+
+Run:
+```
+$ node pico-jarvis
+```
+
+and then open `localhost:5000`.
--- pico-jarvis.js
+++ pico-jarvis.js
@@ -1,1 +1,26 @@
-console.log('Starting Pico Jarvis...');
+const fs = require('fs');
+const http = require('http');
+
+function handler(request, response) {
+    const { url } = request;
+    console.log(`Handling ${url}...`);
+    if (url === '/health') {
+        response.writeHead(200).end('OK');
+    } else if (url === '/' || url === '/index.html') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(fs.readFileSync('./index.html'));
+    } else if (url.startsWith('/chat')) {
+        const parsedUrl = new URL(`http://localhost/${url}`);
+        const { search } = parsedUrl;
+        const question = decodeURIComponent(search.substring(1));
+        const answer = 'Yo man';
+        console.log({ question, answer });
+        response.writeHead(200).end(answer);
+    } else {
+        console.error(`${url} is 404!`)
+        response.writeHead(404);
+        response.end();
+    }
+}
+
+http.createServer(handler).listen(5000);
